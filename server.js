4fc23e75bb9bc9e1e42c4fd41cd207743ae16b6d
Ronald Deguzman <bootcampcoding.ronnie@gmail.com>
2018-06-30 19:17:42 -0700

    proper use of handlebars to format the title and link features in the handlebars page.  Deleted dummy data that was throwing the html organization off.

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ let exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
+// The app name for this application on heroku is "pacific bayou"
 
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
@@ -41,64 +41,93 @@ let collections = ["data"];
 var db = mongojs(databaseUrl, collections);
 
 // This makes sure that any errors are logged if mongodb runs into an issue
-db.on("error", function(error) {
-    console.log("Database Error:", error);
-  });
+db.on("error", function (error) {
+  console.log("Database Error:", error);
+});
 
-  // Routes
+// Routes
 // 1. At the root path, send a simple hello world message to the browser using handlebars
 app.get("/", function (req, res) {
-    res.render("index");
-  });
+  console.log('getting stuffs');
+  db.data.find({}, (err, dbData) => {
+    if (err) throw err;
 
-  // 2. News Related to Scraper Bikes
-  app.get("/news", function(req, res) {
-    request("https://www.eastbaytimes.com", function(error, response, html) {
-  
+    var context = {
+      articles: dbData,
+    };
+    res.render("index", context);
+  });
+});
+
+// 2. News Related to Scraper Bikes
+app.get("/scrape", function (req, res) {
+  request("https://www.eastbaytimes.com", function (error, response, html) {
+
     // Load the HTML into cheerio and save it to a variable
     // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
     let $ = cheerio.load(html);
-  
-  
+
+
     // Select each element in the HTML body from which you want information.
     // NOTE: Cheerio selectors function similarly to jQuery's selectors,
     // but be sure to visit the package's npm page to see how it works
-    $(".entry-title").each(function(i, element) {
-  
+    $(".entry-title").each(function (i, element) {
+
       let link = $(element).children().attr("href");
       let title = $(element).children().text();
-    //   let picture = $(element).children().attr("")
+      //   let picture = $(element).children().attr("")
 
-  
+
       // Save these results in an object that we'll insert into database collection
-      let stuff = [];
       let info = db.data.insert({
         title: title,
         link: link,
       });
-      stuff.push(info);
-      console.log("This is stuff", stuff);
-    });
-    res.json({message: 'hi'});
-  });  
-  });
+      // Put title and link into an object called infoObject
+      let articleInfo =
+      {
+        title: title,
+        link: link,
+      }
+      console.log("THIS IS IMPORTANT", articleInfo);
+      console.log("title of article only", articleInfo.title);
+      console.log("url of article only", articleInfo.link);
 
-  // 3. At the "/all" path, display every entry in the data collection
-app.get("/all", function(req, res) {
-    // Query: In our database, go to the data collection, then "find" everything
-    db.data.find({}, function(err, found) {
-      // Log any errors if the server encounters one
-      if (err) {
-        console.log(err);
-      }
-      // Otherwise, send the result of this query to the browser
-      else {
-        res.json(found);
-      }
+
+      //   stuff.push(info);
+      //   console.log("This is stuff", info);
     });
+    // res.json({ message: 'Succesfully Scraped' });
+        res.json({ articleInfo });
+        alert("Successfully Scraped");
+
+
   });
-  
+});
+
+// 3. At the "/all" path, display every entry in the data collection
+app.get("/news", function (req, res) {
+  // Query: In our database, go to the data collection, then "find" everything
+  db.data.find({}, function (err, found) {
+    // created array to push found into stuff
+    let stuff = [];
+    stuff.push(found);
+    // console.log(stuff)
+    console.log("HERE AT THE ARTICLES: titles + urls", stuff);
+    // Log any errors if the server encounters one
+    if (err) {
+      console.log(err);
+    }
+    // Otherwise, send the result of this query to the browser
+    else {
+      //   res.render("index", stuff)
+      res.json(stuff);
+    }
+  });
+});
+
+
 // Set the app to listen on port 3000
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("App running on port 3000!");
 });
